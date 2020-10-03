3f0481c4ed687cfa4ee5e3efee4773829b47af52
Francisndungu2 <francianmbugua@gmail.com>
2020-10-03 19:18:35 +0300

    addalert

--- js/script.js
+++ js/script.js
@@ -13,5 +13,19 @@ function getName() {
 
     if (year == "" || year.length < 4 || year.length > 4) {
         alert("Hey you need to enter a valid year!");
+    } else if (month < 0 || month > 12) {
+        alert("Hey you need to enter a valid month!");
+    } else if (dayOfMonth < 0 || dayOfMonth > 31) {
+        alert("hey you need to re-enter a valid day!");
+    } else if (male.checked == false && female.checked == false) {
+        alert("you need to specify your gender!");
+    } else if (male.checked == true) {
+        result.innerHTML = "Hey" + maleNames[birthday] + ".Well, since you were born on a " + days[birthDay] +
+            "that's your Ghanaian name.";
+    } else if (female.checked == true) {
+        result.innerHTML = "Hey" + femaleNames[birthday] + ". Well, since you were born on a " + days[birthDay] + " that's your Ghanaian name.";
+    } else {
+        alert("Come on Akan, just run!")
+
     }
 }
