04bd98b27a380c97b8034b6a3e626667e315e0f5
Francisndungu2 <francianmbugua@gmail.com>
2020-10-03 17:22:14 +0300

    addyearmonthday js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function getName() {
+    var year = document.getElementById("yob").value;
+    var month = document.getElementById("month").value;
+    var dayofmonth = document.getElementById("day").value
+}
