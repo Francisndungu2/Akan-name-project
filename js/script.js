2e43839b65688bbeebe4d5432bd2bcde168498e2
Francisndungu2 <francianmbugua@gmail.com>
2020-10-03 17:57:11 +0300

    addmalenames days femalename

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 function getName() {
     var year = document.getElementById("yob").value;
     var month = document.getElementById("month").value;
-    var dayofmonth = document.getElementById("day").value
+    var dayofmonth = document.getElementById("day").value;
+    var date = new Date(`${year}-${month}-${dayofmonth}`);
+    var birthday = date.getDate();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    var result = document.getElementById("result");
+    var maleNames = ["Kwasi", "Kwandwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }
